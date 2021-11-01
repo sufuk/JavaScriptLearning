c8d6e81baeb1a5ed399f62e0bdea9c0a4d16b17c
sufuk <sufukguler@gmail.com>
2021-11-01 21:18:34 +0300

    Battleship project

--- 02-BattleShipGame/script.js
+++ 02-BattleShipGame/script.js
@@ -1,12 +1,50 @@
-function myFunc() {
-    alert("This is an alert");
-}
-var index = 0;
-while (index < 5){
-    index++;
-    document.write("A line<br>")
-    console.log(index)
-}
+// Ship coordinates
+var location1 = Math.floor(Math.random() * 5);
+var location2 = location1 + 1;
+var location3 = location1 + 2;
 
+// User input
+var guess;
 
-myFunc()
+// Number of hits
+var hits = 0;
+
+// Number of guesses
+var guesses = 0;
+
+var isSunk = false;
+
+while (!isSunk) {
+    // Get user guess
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+
+    if (guess == null) { // User pressed cancel button
+        alert("Goodbye");
+        break;
+    } else {
+        if (guess < 0 || guess > 6) { // User pressed invalid input
+            alert("Please enter a valid cell number!");
+        } else {
+            guesses++;
+
+            if (guess == location1 || guess == location2 || guess == location3) {
+                hits++;
+
+                if (hits === 3) {
+                    isSunk = true;
+                    alert("You sank my battleship!");
+                    break;
+                } else {
+                    alert("You hit my battleship!");
+                }
+            } else {
+                alert("Your input is invalid!");
+            }
+
+        }
+    }
+
+}
+var stats = "You took " + guesses + " guesses to sink the battleship, " +
+    "which means your shooting accuracy was " + (3 / guesses);
+alert(stats);
