f4cca3c478ce0ecf28d61bdc853fc9c7192f1c2b
sufuk <sufukguler@gmail.com>
2022-02-03 20:34:13 +0300

    Chapter 10 update

--- 010-AdvancedFunctions/script.js
+++ 010-AdvancedFunctions/script.js
@@ -9,6 +9,28 @@ var passengers = [
 window.onload = init;
 function init(){
 
+    // Check if all passengers paid
+    for (var i = 0; i < passengers.length; i++) {
+        if (!isPaid(passengers[i])){
+            // Remove passenger from flight
+            passengers.splice(i, 1);
+        }
+    }
+
+    for (var j = 0; j < passengers.length; j++) {
+        console.log(passengers[j]);
+        if (passengers[j].nationality === "US" || passengers[j].nationality === "UK"){
+            sayIt(englishTranslator);
+        }
+        else if (passengers[j].nationality === "TR"){
+            sayIt(turkishTranslator);
+        }
+        else if (passengers[j].nationality === "DE"){
+            sayIt(germanTranslator);
+        }
+
+    }
+
 
 
 }
@@ -23,8 +45,17 @@ function sayIt(translator) {
     var phrase = translator("Hello");
     alert(phrase);
 }
-function hawaiianTranslator(word) {
-    if (word === "Hello") return "Aloha";
-    if (word === "Goodbye") return "Aloha";
+
+function englishTranslator(word) {
+    if (word === "Hello") return "Hello";
+    if (word === "Goodbye") return "Goodbye";
 }
-sayIt(hawaiianTranslator);
+
+function turkishTranslator(word) {
+    if (word === "Hello") return "Merhaba";
+    if (word === "Goodbye") return "Gorusuruz";
+}
+function germanTranslator(word) {
+    if (word === "Hello") return "Hallo";
+    if (word === "Goodbye") return "Auf Wiedersehen";
+}
